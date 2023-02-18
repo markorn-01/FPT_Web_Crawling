e217c83962fcc01133b79c862323d6ef2ceabea1
Minh <minh17301@gmail.com>
2023-02-18 13:23:48 +0700

    get macbook info

--- CrawWeb/src/CrawlingData.java
+++ CrawWeb/src/CrawlingData.java
@@ -8,26 +8,41 @@ import org.jsoup.nodes.Element;
 
 
 public class CrawlingData {
-    public static void main(String[] args) throws Exception {
-        String link = "https://fptshop.com.vn/apple/iphone";
+    public static void crawlData(String link, List<Pair> productInfo) throws Exception{     
         Document doc = Jsoup.connect(link).timeout(50000).get();
-        List<Pair> productInfo = new ArrayList<Pair>();
         ArrayList<Element> products = doc.getElementsByClass("product-grid productitem").get(0).getElementsByClass("product product-grid__item product--absolute");
         for (Element product : products) {
             String name = product.getElementsByClass("product_name").get(0).text();
-            Integer price = Integer.parseInt(product.getElementsByClass("product_progress").get(0).text().replaceAll("[.₫\\s]", ""));
+            Integer price = 0;
+            if (product.getElementsByClass("product_info").first().children().hasClass("product_timing")){
+                price = Integer.parseInt(product.getElementsByClass("product_progress").get(0).text().replaceAll("[.₫\\s]", ""));
+            }
+            if (product.getElementsByClass("product_info").first().children().hasClass("product__price")) {
+                price = Integer.parseInt(product.getElementsByClass("price").get(0).text().replaceAll("[.₫\\s]", ""));
+            }
             productInfo.add(new Pair(name, price));
         }
+        
+    }
+    public static void sorting(List<Pair> productInfo){
         Collections.sort(productInfo, new Comparator<Pair>() {
             @Override
             public int compare(Pair o1, Pair o2) {
                 if(o1.getName().compareTo(o2.getName()) != 0){
-                    return o1.getName().compareTo(o2.getName());
+                    return o2.getName().compareTo(o1.getName());
                 }
-                return o1.getPrice().compareTo(o2.getPrice());
+                return o2.getPrice().compareTo(o1.getPrice());
             }
         });
-        for (Pair pair : productInfo) {
+    }
+    public static void main(String[] args) throws Exception {
+        String linkIphone = "https://fptshop.com.vn/apple/iphone";
+        String linkMacbook = "https://fptshop.com.vn/apple/macbook";
+        List<Pair> list = new ArrayList<Pair>();
+        crawlData(linkIphone, list);
+        crawlData(linkMacbook, list);
+        sorting(list);
+        for (Pair pair : list) {
             System.out.println(pair.getName() + ": " + pair.getPrice());
         }
     }
